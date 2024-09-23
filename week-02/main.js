667e2f14d8c2ed508c6854f403abc7eab190b969
amyhsu <amy991225@gmail.com>
2024-09-23 11:25:58 +0800

    [feat][test] stack.js, main.js: stack done + test

--- week-02/main.js
+++ week-02/main.js
@@ -1,12 +1,43 @@
-// main.js
-// TODO 以 Module 的方式匯入，例如:
 import Stack from './stack.js';
 
 let stack = new Stack();
-stack.print();
 
+// [Test]
+
+// check: .peek()、.isEmpty()、.size()、.print()
+function stackInfo(stack) {
+    console.log(`peek: ${stack.peek()}`);
+    console.log(`isEmpty: ${stack.isEmpty()}`);
+    console.log(`size: ${stack.size()}`);
+    console.log("stack:");
+    stack.print();
+    console.log();
+}
+
+// check: constructor，初始狀態
+stackInfo(stack);
+
+// check: .push(element)
 stack.push(5);
 stack.push(8);
-stack.print();
+stackInfo(stack);
 
-// TODO: 應該還要做哪些測試，以驗證自己開發的 stack 是沒有問題的？
+// check: .pop()
+console.log(`pop ${stack.pop()}`);
+stackInfo(stack)
+
+// check: .pop()，當 pop 次數超過元素個數
+console.log(`pop ${stack.pop()}`);
+console.log(`pop ${stack.pop()}`);
+stackInfo(stack)
+
+stack.push(1);
+stack.push(3);
+stackInfo(stack)
+
+// check: .clear()
+stack.clear();
+stackInfo(stack);
+
+stack.push(2);
+stackInfo(stack);
--- week-02/stack.js
+++ week-02/stack.js
@@ -1,7 +1,10 @@
-// stack.js
-// 完成以下 TODO 的部分，並且以 Module 的方式匯出 (ESM)
 export default class Stack {
-    // TODO: # 有特別的意思嗎？請以註解回覆。
+    /* 
+       在 class 的 property 或 method 前加 # 符號，
+       表示該 property 或 method 的存取權限為 private。
+       只有在該 class 的內部能夠直接存取和呼叫，
+       在該 class 以外以及繼承自此 class 的其他 class 中皆無法直接存取和呼叫。
+    */
     #items;
    
     constructor() {
@@ -10,36 +13,38 @@ export default class Stack {
    
     // 在 stack 頂部加入元素i
     push(element) {
-        // TODO
+        this.#items.push(element);
     }
    
     // 移除並回傳 stack 頂部的元素
     pop() {
-        // TODO
+        if (!this.isEmpty()) return this.#items.pop();
+        else return "(stack is empty)"
     }
    
     // 回傳 stack 頂部的元素，但不移除它
     peek() {
-        // TODO
+        if (!this.isEmpty()) return this.#items.at(-1);
+        else return "(stack is empty)"
     }
    
     // 檢查 stack 是否為空
     isEmpty() {
-        // TODO
+        return this.#items.length == 0;
     }
    
     // 回傳 stack 中元素的個數
     size() {
-        // TODO
+        return this.#items.length;
     }
    
     // 清空 stack
     clear() {
-        // TODO
+        this.#items.length = 0;
     }
    
     // 印出 stack 內容（可選）
     print() {
-        // TODO
+        console.log(this.#items);
     }
 }
