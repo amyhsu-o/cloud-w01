709394acd1aafb7a20d694b45390f0d3cac1eb28
amyhsu <amy991225@gmail.com>
2024-09-23 12:49:44 +0800

    [feat] sum.js: array sum, 1+2+...+n done

--- week-02/sum.js
+++ week-02/sum.js
@@ -1,6 +1,32 @@
-// ary: number array
+// Q1: sum all elements in ary；ary: number array
 function sum(ary) {
-    // TODO: sum all elements in ary
+    // solution 0: .reduce()
+    return ary.reduce((total, num) => total + num, 0);
 }
-   
-console.log(sum([1, 5, 3, 2])); // 11
+
+function sum1(ary) {
+    // solution 1: recursion
+    if (ary.length > 0) return ary[0] + sum1(ary.slice(1));
+    else return 0;
+}
+
+function sum2(ary) {
+    // solution 2: .forEach()
+    let total = 0;
+    ary.forEach(function(num) {
+        total += num;
+    })
+    return total;
+}
+
+// Q2: 1 + 2 + ... + n
+function sumToN(n) {
+    // solution 0: 梯形公式
+    return (n+1) * n / 2;
+}
+
+function sumToN1(n) {
+    // solution 1: recursion
+    if (n > 0) return n + sumToN1(n-1);
+    else return 0;
+}
