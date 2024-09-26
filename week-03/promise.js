722403a24df2d852ad5a6509d25b8c76dde0fdb2
amyhsu <amy991225@gmail.com>
2024-09-27 00:06:39 +0800

    [feat] promise.js: doJob by promise - solution 2, done

--- week-03/promise.js
+++ week-03/promise.js
@@ -33,4 +33,23 @@ function doAllJobs1() {
     });
 }
 
+// solution 2: async & await
+async function doAllJobs2(jobs) {
+  console.log(`開始工作 at ${new Date().toISOString()}`);
+
+  for (let job of jobs) {
+    console.log(await doJob(job.name, job.time));
+  }
+}
+
+// execute doAllJobs function
+let jobs = [
+  { name: "刷牙", time: 1000 },
+  { name: "吃早餐", time: 3000 },
+  { name: "寫功課", time: 1000 },
+  { name: "吃午餐", time: 2000 },
+];
+
+// uncomment one solution at a time
 doAllJobs1(); // solution 1: .then()
+// doAllJobs2(jobs); // solution 2: async & await
